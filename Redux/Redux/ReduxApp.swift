4b66ec8bfceff4f59ad300741c2da3ac786817d1
ShuZik <iShuZik@gmail.com>
2024-03-28 19:20:23 +0200

    Make "ToDo" App

---
+++ Redux/Redux/ReduxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReduxApp.swift
+//  Redux
+//
+//  Created by ShuZik on 28.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ReduxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TodoApp/TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by ShuZik on 28.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
